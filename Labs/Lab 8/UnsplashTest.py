b7a2ddb76d34db4b4fa6613e8118f6d06b42be72
vitluchko <98816838+vitluchko@users.noreply.github.com>
2023-05-18 18:30:30 +0300

    Open the field for searching by photo and upload the photo

--- Labs/Lab 8/UnsplashTest.py
+++ Labs/Lab 8/UnsplashTest.py
@@ -9,8 +9,8 @@ driver.get("https://unsplash.com/")
 btn_input = driver.find_element(By.XPATH, "//button[@type='button']")
 btn_input.click()
 
-# Пауза на 3 секунд
-time.sleep(3)
+# Пауза на 1 секунд
+time.sleep(1)
 
 img_input = driver.find_element(By.XPATH, "//input[@type='file']")
 img_input.send_keys("C:/Users/Win7/PycharmProjects/pythonProject/images/zolkin.jpg")
