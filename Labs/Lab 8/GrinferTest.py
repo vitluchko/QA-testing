3168b4a45b1f3ac49c21dbc6808487ffa75109f5
vitluchko <98816838+vitluchko@users.noreply.github.com>
2023-05-18 18:23:16 +0300

    Create GrinferTest.py
    Python + Selenium auto tests

---
+++ Labs/Lab 8/GrinferTest.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+driver = webdriver.Chrome()
+
+driver.get("https://grinfer.com/")
+
+btn_log_in = driver.find_element(By.XPATH, "//a[@class='sc-2aalce-2 dvzzdp']").click()
+
+input_email = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "email"))).send_keys("test@test.com")
+
+input_password = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "password"))).send_keys("1234567")
+
+click_btn_log_in = driver.find_element(By.XPATH, "//button[@type='submit']").click()
+
+# Пауза на 20 секунд
+time.sleep(20)
+
+# Закриття веб-сторінки та браузера
+driver.quit()
